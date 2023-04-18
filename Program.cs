65ac1649d27a0f978efa30c8b8009f9294242ccf
Дмитрий Матохин <argizol@mail.ru>
2023-04-18 17:17:35 +0300

    Добавлена проверка на пустоту строки

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+string path = @"D:/Загрузки работа/data/Текстовый документ.txt";
+using StreamReader sr = new StreamReader(path);
+using StreamWriter sw = new StreamWriter(@"D:/Загрузки работа/data/result.txt");
+StringBuilder sb = new StringBuilder();
+while (!sr.EndOfStream)
+{
+    string line = sr.ReadLine()!;
+    if (line != null)
+        sb.Append(line + ';');
+    else continue;
+    
+}
+sw.Write(sb);
