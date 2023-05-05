04acc7c4e7719c6c9242843ef8c1d8e76df4cb49
Дмитрий Матохин <argizol@mail.ru>
2023-05-05 13:42:08 +0300

    Изменено условие выборки строк

--- ReadExcel.cs
+++ ReadExcel.cs
@@ -6,10 +6,10 @@ using System.IO;
 
 namespace ReadAllLinesConcatInOneStringAndSetDelimiter
 {
-   
+
     internal class ReadExcel
     {
-         
+
         public static void GetDataFromExcel()
         {
 
@@ -25,7 +25,7 @@ namespace ReadAllLinesConcatInOneStringAndSetDelimiter
                 {
                     //Get the path of specified file
                     path = openFileDialog.FileName;
-                    
+
                 }
             }
 
@@ -37,30 +37,28 @@ namespace ReadAllLinesConcatInOneStringAndSetDelimiter
             {
                 ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
 
-                if (worksheet is not null)
-                {
-                    int limiter = worksheet.Dimension.End.Row;
-                    for (int i = 2; i < limiter; i++)
-                    {
-                        //Получаем значения, если для числового значения null меняем на 0
-
-                        double PC = (double?)worksheet.Cells[$"AV{i}"].Value ?? 0.0;
-                        double PPDL = (double?)worksheet.Cells[$"X{i}"].Value ?? 0.0;
-                        double PPD = (double?)worksheet.Cells[$"Y{i}"].Value ?? 0.0;
-
-                        string IsGM = worksheet.Cells[$"AY{i}"].Value.ToString() ?? " ";
-
-                        //Собираем строку id промо если соответствует условиям
-                        if (PC >= 20.0 && PPD <= PPDL && IsGM.Equals("yes"))
-                        {
-                            sb.Append(worksheet.Cells[$"A{i}"].Value).Append(';');
-                        }
-                    }
-                }
-                else
+                if (worksheet is null)
                 {
                     Console.WriteLine("No worksheets in file");
                     return;
+                }
+
+                int limiter = worksheet.Dimension.End.Row;
+                for (int i = 2; i < limiter; i++)
+                {
+                    //Получаем значения, если для числового значения null меняем на 0
+
+                    double PC = (double?)worksheet.Cells[$"AV{i}"].Value ?? 0.0;
+                    double PPDL = (double?)worksheet.Cells[$"X{i}"].Value ?? 0.0;
+                    double PPD = (double?)worksheet.Cells[$"Y{i}"].Value ?? 0.0;
+
+                    string IsGM = worksheet.Cells[$"AY{i}"].Value.ToString() ?? " ";
+
+                    //Собираем строку id промо если соответствует условиям
+                    if ((PC >= 20.0 || PPD <= PPDL) && IsGM.Equals("yes"))
+                    {
+                        sb.Append(worksheet.Cells[$"A{i}"].Value).Append(';');
+                    }
                 }
 
                 //Пишем строку с id промо в файл, разделитель ;
